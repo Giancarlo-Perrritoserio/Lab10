298bb3d357352645bc3e07baeaaaed9a1ddbba74
Giancarlo <giancarlo.munoz@tecsup.edu.pe>
2024-10-07 14:41:01 -0500

    Todos los datos commit
    me olvide de hacer commmit en cada parte.

---
+++ app/src/main/java/com/example/lab10/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.lab10
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab10.ui.theme.Lab10Theme
+import com.example.lab10.viewmodel.SeriesApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SeriesApp()
+        }
+    }
+}
+
